9652cebd316f0e59ffd5c0056af5cd135a4f31f6
cezarltsv <cezarltsv@gmail.com>
2024-12-30 02:07:11 -0300

    feat: create a object of value to uuid generate and validate

---
+++ src/shared/domain/values-objects/__test__/uuid.vo.spec.ts
@@ -0,0 +1,29 @@
+import { invalidUuidError, Uuid } from "../uuid.vo"
+import { validate as uuidValidate } from "uuid";
+
+describe("Domain - Uuid UnitTest", () => {
+
+    const validateSpy = jest.spyOn(Uuid.prototype as any, "validate");
+
+    describe("ValueObject", () => {
+        test("Should  throw error when uuid is invalid", () => {
+            const result = () => new Uuid("231");
+            expect(result).toThrow(new invalidUuidError());
+            expect(validateSpy).toHaveBeenCalled();
+        })
+    
+        test("Should create a uuid when not provided", () => {
+            const result = new Uuid();
+            expect(result.id).toBeDefined();
+            expect(uuidValidate(result.id)).toBeTruthy();
+            expect(validateSpy).toHaveBeenCalled();
+        })
+
+        test("Should create a uuid when provided", () => {
+            const uuid = "0899d2a1-fde5-472a-9a21-c5826dc4f081";
+            const result = new Uuid(uuid);
+            expect(result.id).toBe(uuid);
+            expect(validateSpy).toHaveBeenCalled();
+        })
+    })
+})
---
+++ src/shared/domain/values-objects/uuid.vo.ts
@@ -0,0 +1,25 @@
+import { ValueObject } from "../values-object";
+import { v4 as idv4, validate as uuidValidate  } from "uuid";
+
+export class Uuid extends ValueObject {
+    public readonly id: string;
+    constructor(id?: string){
+        super();
+        this.id = id || idv4();
+        this.validate();
+    }
+
+    private validate(){
+        const isValid = uuidValidate(this.id);
+        if(!isValid) {
+            throw new invalidUuidError();
+        }
+    }
+}
+
+export class invalidUuidError extends Error {
+    constructor(message?: string){
+        super(message || "Id must be a valid uuid");
+        this.name = this.constructor.name;
+    }
+}

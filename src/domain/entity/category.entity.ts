fe320a582166dbc0100b7ea3f9160eb976692ef0
cezarltsv <cezarltsv@gmail.com>
2024-12-30 00:28:16 -0300

    feat: create domain category

---
+++ src/domain/entity/category.entity.ts
@@ -0,0 +1,59 @@
+export type CategoryConstructorProps= {
+    categoryId?: string;
+    name: string;
+    description?: string | null;
+    isActive?: boolean;
+    createdAt?: Date;
+}
+
+export type CategoryCreateCommand = {
+    name: string;
+    description?: string;
+    isActive?: boolean
+}
+
+export class Category {
+    categoryId: string;
+    name: string;
+    description: string | null;
+    isActive: boolean;
+    createdAt: Date;
+
+    constructor(props: CategoryConstructorProps) {
+        this.categoryId = props.categoryId ?? "";
+        this.name = props.name;
+        this.description = props.description ?? null;
+        this.isActive = props.isActive ?? true;
+        this.createdAt = props.createdAt ?? new Date();
+    }
+
+    static create(props: CategoryCreateCommand) {
+        return new Category(props);
+    }
+
+    changeName(name: string): void {
+        this.name = name;
+    }
+
+    changeDescription(description: string): void {
+        this.description = description;
+    }
+
+    activate() {
+        this.isActive = true;
+    }
+
+    deactivate() {
+        this.isActive = false;
+    }
+
+    toJson(){
+        return {
+            categoryId: this.categoryId,
+            name: this.name,
+            description: this.description,
+            isActive: this.isActive,
+            createdAt: this.createdAt,
+        }
+    }
+}
